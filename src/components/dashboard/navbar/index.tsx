3c4cbf71073f57f18f158c87a0983e56e96dc1d4
Alisson Delatim <alisson.delatim@hotmail.com>
2025-01-19 21:47:27 -0300

    Correção Navbar

--- src/components/dashboard/navbar/index.tsx
+++ src/components/dashboard/navbar/index.tsx
@@ -8,22 +8,24 @@ type NavbarProps = {
 
 export const Navbar = ({ openModal, setOpenModal }: NavbarProps) => {
   return (
-    <nav className="fixed left-0 right-0 top-0 flex h-[10%] items-center justify-between border-b border-b-zinc-100 bg-zinc-50 px-6 shadow-md">
-      <div className="flex flex-row gap-3">
-        <Menu
-          className="hidden h-8 w-10 cursor-pointer border lg:block"
-          onClick={() => setOpenModal(!openModal)}
-        />
-        <img src={LogoIcon} alt="Icon" className="hidden lg:block" />
+    <nav className="fixed left-0 right-0 top-0 flex h-16 items-center border-b border-b-zinc-100 bg-zinc-50 px-6 shadow-md">
+      <div className="flex w-full items-center justify-between">
+        <div className="flex flex-row gap-3">
+          <Menu
+            className="hidden h-8 w-10 cursor-pointer border lg:block"
+            onClick={() => setOpenModal(!openModal)}
+          />
+          <img src={LogoIcon} alt="Icon" className="hidden lg:block" />
+        </div>
+        <ul className="flex flex-row items-center gap-6">
+          <li>
+            <SunMoon className="h-8 w-8 cursor-pointer" />
+          </li>
+          <li>
+            <CircleUser className="h-8 w-8 cursor-pointer" />
+          </li>
+        </ul>
       </div>
-      <ul className="ml-auto flex flex-row gap-6">
-        <li>
-          <SunMoon className="h-8 cursor-pointer" />
-        </li>
-        <li>
-          <CircleUser className="h-8 cursor-pointer" />
-        </li>
-      </ul>
     </nav>
   )
 }
